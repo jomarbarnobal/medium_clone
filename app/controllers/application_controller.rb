ffa15b6142ffbae5311da0674a514e1fe2fdb46a
jomarbarnobal <jomar.barnobal@outlook.com>
2017-12-03 01:12:21 -0500

    add username filed in user session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+    def configured_permitted_parameters
+      devise_parameter_sanitize.for(:sign_up) { |u| u.permit(:username, :emmail, :password, :password_confirmation, :remember_me)}
+      devise_parameter_sanitize.for(:sign_in) { |u| u.permit(:login, :username, :remember_me)}
+      devise_parameter_sanitize.for(:account_update) { |u| u.permit(:username, :emmail, :password, :password_confirmation, :current_password)}
+    end
+
 end
